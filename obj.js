8453eae9cac629aa877e22c9e629442becf70e6a
Ultraman Tiga <husnulhamidiah@gmail.com>
2024-03-07 09:04:28 +0700

    Add object js for development

---
+++ obj.js
@@ -0,0 +1,15 @@
+const ultraman = {
+  name: "tiga",
+  height: 100,
+  isDead: false,
+  weapons: ["sword", "shield", "lasso"],
+  attack: () => {
+    console.log("sword attack!")
+  },
+  beam: (number) => console.log("spacium ray beam " + number + "%")
+}
+
+ultraman.attack()
+ultraman.beam(80)
+
+console.log(ultraman)
